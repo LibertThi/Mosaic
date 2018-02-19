5c49479a9b03959e958da65782f9c3dcc8121240
LibertThi <thibault.libert@rpn.ch>
2018-02-19 15:25:57 +0100

    Add implementation of a high res option

--- createMosaic.php
+++ createMosaic.php
@@ -8,24 +8,27 @@ use ColorThief\ColorThief;
 $imagine = new Imagine\Gd\Imagine();
 
 // Options:
-$opt = getopt("",["rows:", "columns:", "img:", "tilesize:"]);
+$opt = getopt("",["rows:", "columns:", "img:", "tilesize:", "tileres:"]);
 /*
 --columns <int> : Number of columns for the mosaic. Overrides Rows if both are used
 --rows <int> : Number of rows for the mosaic. Overridden by Columns if both are used
 --img <path> : Path of the image to process. Must be set
 --tilesize <int> : Set tilesize for the mosaic. Overridden by columns and rows if they are defined
+--tileres <int> : Set the resolution (in pixels) of each tile. Beware, as it uses a lot of memory !
 */
 $columns = 0;
 $rows = 0;
 $baseImgPath = "";
 $tileSize = 0;
+$tileRes = 0;
 
 if (!$opt){
     echo "Usage: php [script] [options]\n";
     echo "--columns <int> : Number of columns for the mosaic. Overrides Rows if both are used
 --rows <int> : Number of rows for the mosaic. Overridden by Columns if both are used
 --img <path> : Path of the image to process. Must be set
---tilesize <int> : Set tilesize for the mosaic. Overridden by columns or rows if any is defined\n";
+--tilesize <int> : Set tilesize for the mosaic. Overridden by columns or rows if any is defined
+--tileres <int> : Set the resolution (in pixels) of each tile. Beware, as it may exceed maximum memory !\n";
     exit;
 }
 // Set "configuration" based on options
@@ -49,6 +52,10 @@ if (isset($opt['rows']) and intval($opt['rows']) != 0 and $columns == 0){
 }
 if (isset($opt['tilesize']) and intval($opt['tilesize']) != 0 and $columns == 0 and $rows == 0){
     $tileSize = (int) $opt['tilesize'];
+}
+if (isset($opt['tileres']) and intval($opt['tileres']) != 0){
+    $tileRes = $opt['tileres'];
+    ini_set('memory_limit','3072M'); // Change here memory limit. Can try with -1 but it's BAD
 }
 
 else if (!is_file($baseImgPath)){
@@ -95,10 +102,19 @@ if ($tileSize == 0){
     }
 }
 
+// Adapt scaling is using High res.
+// Each tile will be larger, resulting in a bigger resolution mosaic
+if ($tileRes != 0){
+    $scaling = $tileRes / $tileSize;
+}
+else{
+    $scaling = 1;
+}
+
 $columns = ceil($baseImgSize[0] / $tileSize);
 $rows = ceil($baseImgSize[1] / $tileSize);
-$mosaicWidth = $tileSize * $columns;
-$mosaicHeight = $tileSize * $rows;
+$mosaicWidth = $tileSize * $columns * $scaling;
+$mosaicHeight = $tileSize * $rows * $scaling;
 $pattern = "/(\w+?)\.(\w*)/";
 $mosaicImgPath = preg_replace($pattern, "$1-" . time() . ".$2", $baseImgPath);
 $mosaicSize = new Imagine\Image\Box($mosaicWidth,$mosaicHeight);
@@ -108,9 +124,9 @@ $pool = new Pool(100, 'Connection', ["root","","mosaic"]);
 $datas = [];
 
 // vertical
-for ($y = 0; $y < $mosaicHeight; $y += $tileSize){
+for ($y = 0; $y < $mosaicHeight / $scaling ; $y += $tileSize){
     // horizontal
-    for ($x = 0; $x < $mosaicWidth; $x += $tileSize){
+    for ($x = 0; $x < $mosaicWidth / $scaling ; $x += $tileSize){
         // datas to pass to each thread
         $data = new Threaded();
         $data->x = $x;
@@ -129,17 +145,17 @@ $pool->shutdown();
 // create mosaic from all datas
 echo "Creating mosaic...\n";
 foreach ($datas as $data){
-    $point = new Imagine\Image\Point($data->x,$data->y);
+    $point = new Imagine\Image\Point($data->x * $scaling, $data->y * $scaling);
     $imageFromFile = $imagine->open(IMG_PATH . "/$data->imgId.$data->imgExt");
-    $imageFromFile->resize(new Imagine\Image\Box($data->tileSize,$data->tileSize));
+    $imageFromFile->resize(new Imagine\Image\Box($data->tileSize * $scaling, $data->tileSize* $scaling));
     $mosaicImg->paste($imageFromFile,$point);
 }
 // crop to fit base img and save
-$mosaicImg = $mosaicImg->crop(new Imagine\Image\Point(0,0), new Imagine\Image\Box($baseImgSize[0],$baseImgSize[1]));
+$mosaicImg = $mosaicImg->crop(new Imagine\Image\Point(0,0), new Imagine\Image\Box($baseImgSize[0] * $scaling, $baseImgSize[1] * $scaling));
 $mosaicImg->save($mosaicImgPath);
 
 // output total time spent
 $endTime = microtime(true);
 $timeSpent = date("H:i:s", $endTime - $startTime);
-echo "Mosaic created on $mosaicImgPath !\n$timeSpent to create\n";
+echo "Mosaic created on '$mosaicImgPath' !\n$timeSpent to create\n";
 ?>
